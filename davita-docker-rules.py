eed1616b483e3305bca51539e293e3bef1823775
Tommy McNeely <thomas.mcneely@davita.com>
2021-05-21 08:08:22 -0600

    test-docker!

--- davita-docker-rules.py
+++ davita-docker-rules.py
@@ -4,14 +4,14 @@ from artifactory_cleanup.rules import CleanupPolicy
 RULES = [
     CleanupPolicy(
         'Feature Branches',
-        rules.repo('davita-docker'),
+        rules.repo('test-docker'),
         rules.delete_docker_images_not_used(days=30),
         rules.delete_docker_image_if_value_in_property('docker.label.branch', ['develop','future-develop','unicorn-develop'], False)
         ## PROTECTED BRANCHES: develop, unicorn-develop, future-develop, bugfix/RELEASE*
     ),
     CleanupPolicy(
         'Protected Branches',
-        rules.repo('davita-docker'),
+        rules.repo('test-docker'),
         rules.delete_docker_images_not_used(days=365),
         rules.delete_docker_image_if_value_in_property('docker.label.branch', ['develop','future-develop','unicorn-develop'], True)
     )
