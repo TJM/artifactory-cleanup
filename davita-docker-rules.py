feff5a75d8faf0558f5fb3d81d244aa4fd62857a
Tommy McNeely <thomas.mcneely@davita.com>
2021-05-24 20:41:53 -0600

    Add unabeled images rule

--- davita-docker-rules.py
+++ davita-docker-rules.py
@@ -4,15 +4,21 @@ from artifactory_cleanup.rules import CleanupPolicy
 RULES = [
     CleanupPolicy(
         'Feature Branches',
-        rules.repo('test-docker'),
+        rules.repo('davita-docker'),
         rules.delete_docker_images_not_used(days=30),
         rules.delete_docker_image_if_value_in_property('docker.label.branch', ['develop','future-develop','unicorn-develop'], False)
         ## PROTECTED BRANCHES: develop, unicorn-develop, future-develop, bugfix/RELEASE*
     ),
     CleanupPolicy(
         'Protected Branches',
-        rules.repo('test-docker'),
+        rules.repo('davita-docker'),
         rules.delete_docker_images_not_used(days=365),
         rules.delete_docker_image_if_value_in_property('docker.label.branch', ['develop','future-develop','unicorn-develop'], True)
-    )
+    ),
+    CleanupPolicy(
+        'Unlabeled Images',
+        rules.repo('davita-docker'),
+        rules.delete_docker_images_not_used(days=365),
+        rules.delete_docker_image_if_value_in_property('docker.label.branch', [], True, True)
+    ),
 ]
